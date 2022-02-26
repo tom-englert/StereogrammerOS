adb7599e7ab4055d98d68c7be9d13519a7c14a0c
tom-englert <mail@tom-englert.de>
2022-02-26 16:02:51 +0100

    Use ImageSharp

--- Animations/Program.cs
+++ Animations/Program.cs
@@ -1,36 +1,33 @@
-﻿using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using SixLabors.ImageSharp;
+﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using Color = SixLabors.ImageSharp.Color;
 
-const int width = 1600;
-const int height = 1200;
+//const int width = 1600;
+//const int height = 1200;
+const int width = 800;
+const int height = 600;
 
-var options = new Options();
-
-options.Texture = new Texture(TextureType.Colourdots, (int)options.Separation, (int)options.Separation);
-options.ResolutionX = width;
-options.ResolutionY = height;
+var options = new Options
+{
+    ResolutionX = width,
+    ResolutionY = height,
+};
 
 using Image<Rgba32> gif = new(width, height, Color.Blue);
+
+// var texture = GenerateTexture((int)options.Separation, (int)options.Separation);
+var texture = Image.Load(@"NuclearCoral.jpg");
+var generator = StereogramGenerator.Get(options);
 
 for (var alpha = 0.0; alpha < 2 * Math.PI; alpha += Math.PI / 10)
 {
     var depthMap = GenerateDepthMap(width, height, alpha);
 
-    options.DepthMap = depthMap;
-    var generator = StereogramGenerator.Get(options);
+    var image = generator.Generate(depthMap, texture);
 
-    var bitmap = new BitmapType { Bitmap = generator.Generate() };
-
-    using var bitmapStream = new MemoryStream();
-    bitmap.SaveImage(bitmapStream, FileType.Bmp);
-    bitmapStream.Position = 0;
-
-    using var image = Image.Load(bitmapStream);
     image.Frames.RootFrame.Metadata.GetGifMetadata().FrameDelay = 5;
+
+    // image.SaveAsPng(@"c:\temp\test.png");
 
     gif.Frames.AddFrame(image.Frames.RootFrame);
 }
@@ -50,29 +47,54 @@ var fileName = Path.Combine(folder.FullName, "doc", "SineWave.gif");
 gif.SaveAsGif(fileName);
 
 
-static DepthMap GenerateDepthMap(int resX, int resY, double alpha)
+static Image GenerateDepthMap(int resX, int resY, double alpha)
 {
-    var bytesPerRow = ((resX + 3) >> 2) << 2; // Round up to multiple of 4
-    var pixels = new byte[bytesPerRow * resY];
+    var pixels = new Image<A8>(resX, resY);
 
     var centerX = resX / 2;
     var centerY = resY / 2;
 
     var factor = resX / 20.0;
 
-    for (var y = 0; y < resY; y++)
+    pixels.ProcessPixelRows(accessor =>
     {
-        var rowOffset = y * bytesPerRow;
-
-        for (var x = 0; x < resX; x++)
+        for (var y = 0; y < resY; y++)
         {
-            var dist = Math.Sqrt((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY));
-            var z = (byte)(128 * (Math.Sin(dist / factor + alpha) + 1.0));
-            pixels[rowOffset + x] = z;
-        }
-    }
+            var row = accessor.GetRowSpan(y);
 
-    var bitmap = new WriteableBitmap(resX, resY, 96.0, 96.0, PixelFormats.Gray8, null);
-    bitmap.WritePixels(new Int32Rect(0, 0, resX, resY), pixels, bytesPerRow, 0);
-    return new DepthMap(bitmap);
+            for (var x = 0; x < resX; x++)
+            {
+                var dist = Math.Sqrt((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY));
+                var z = (byte)(128 * (Math.Sin(dist / factor + alpha) + 1.0));
+
+                row[x] = new A8(z);
+            }
+        }
+    });
+
+    return pixels;
+}
+
+static Image GenerateTexture(int resX, int resY)
+{
+    var random = new Random();
+    var pixels = new Image<Rgb24>(resX, resY);
+
+    pixels.ProcessPixelRows(accessor =>
+    {
+        for (var y = 0; y < resY; y++)
+        {
+            var row = accessor.GetRowSpan(y);
+
+            for (var x = 0; x < resX; x++)
+            {
+                var p = (byte)random.Next(256);
+                row[x] = new Rgb24(p, p, p);
+
+                // row[x] = new Rgb24((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
+            }
+        }
+    });
+
+    return pixels;
 }
---
+++ Engine.ImageSharp/StereogramGenerator.cs
@@ -0,0 +1,308 @@
+﻿// Copyright 2012 Simon Booth
+// All rights reserved
+// http://machinewrapped.wordpress.com/stereogrammer/
+//
+// See individual classes for algorithm copyrights
+
+namespace Engine;
+
+/// <summary>
+/// Base class for stereogram generator - does most of the work, but lets subclasses provide
+/// an implementation of a particular algorithm for processing each line of the image
+/// </summary>
+public abstract class StereogramGenerator
+{
+    /// <summary>
+    /// Factory method to get a generator for the appropriate algorithm
+    /// </summary>
+    public static StereogramGenerator Get(Options options)
+    {
+        return new StereogramGeneratorHoroptic(options);
+    }
+
+    /// <summary>
+    /// Constructor caches the important options and does some common setup work
+    /// </summary>
+    public StereogramGenerator(Options options)
+    {
+        _options = options;
+
+        resolutionX = options.ResolutionX;
+        resolutionY = options.ResolutionY;
+        separation = options.Separation;
+        FieldDepth = options.FieldDepth;
+
+        // Bound the depth between 0 and 1... could arguably use higher depth factors on a low-level depthmap, but more danger of a crash from invalid Zs.
+        if (FieldDepth < 0.0)
+            FieldDepth = 0.0;
+
+        if (FieldDepth > 1.0)
+            FieldDepth = 1.0;
+
+        // Cache some intermediaries
+        lineWidth = resolutionX;
+        rows = resolutionY;
+        _depthWidth = lineWidth;
+        _depthScale = 1;
+
+        midpoint = lineWidth / 2;
+
+        bytesPerPixel = sizeof(uint);       // Ugh... relies on Pbgra32 pixel format being 32 bits, which obviously it will be, but it's not exactly pretty is it?
+        bytesPerRow = lineWidth * bytesPerPixel;
+    }
+
+    private readonly Options _options;
+
+    protected bool RemoveHiddenSurfaces => _options.RemoveHiddenSurfaces;
+
+    // Cache the important options as readonly, in the hope it helps with optimisation
+    protected readonly int resolutionX;
+    protected readonly int resolutionY;
+    protected readonly double FieldDepth;
+    protected readonly double separation;
+
+    // Worker variables for generation algorithms...
+    protected readonly int rows;
+    protected readonly int lineWidth;
+    protected readonly int midpoint;
+    protected readonly int bytesPerPixel;
+    protected readonly int bytesPerRow;
+
+    // Depthwidth = line width if we're interpolating depths... subclasses don't need to know that though
+    private readonly int _depthWidth;
+    private readonly int _depthScale;
+
+    // Big buffers to hold data... wasteful but necessary if parallelised
+    private Image<Rgb24> _pixels = null!;
+    private Image<Rgb24> _texturePixels = null!;
+    private Image<A8> _depthBytes = null!;
+
+    // Temp hack for progress report & abort
+    public int GeneratedLines { get; private set; }
+    public int NumLines => rows;
+    public bool abort = false;
+    public long Milliseconds = 0;
+
+    /// <summary>
+    /// Each algorithm operates on a line at a time, so subclasses must implement
+    /// the DoLine function with algorithm appropriate functionality
+    /// </summary>
+    /// <param name="y"></param>
+    protected abstract void DoLine(int y);
+
+    /// Let's see how well C# optimises these... would hope they get inlined at least
+    protected byte GetDepth(int x, int y)
+    {
+        return _depthBytes[x / _depthScale, y].PackedValue;
+    }
+
+    protected float GetDepthFloat(int x, int y)
+    {
+        return GetDepth(x, y) / 255f;
+    }
+
+    protected Rgb24 GetTexturePixel(int x, int y)
+    {
+        return _texturePixels[(x + midpoint) % _texturePixels.Width, y % _texturePixels.Height];
+    }
+
+    protected void SetStereoPixel(int x, int y, Rgb24 pixel)
+    {
+        _pixels[x, y] = pixel;
+    }
+
+    // Just for readability
+    protected static double SquareOf(double x)
+    {
+        return x * x;
+    }
+
+    // Return which of two values is furthest from a mid-point (or indeed any point)
+    protected static int Outermost(int a, int b, int midpoint)
+    {
+        return (Math.Abs(midpoint - a) > Math.Abs(midpoint - b)) ? a : b;
+    }
+
+    /// <summary>
+    /// Helper to calculate stereo separation in pixels of a point at depth Z
+    /// </summary>
+    /// <param name="z"></param>
+    /// <returns></returns>
+    protected double Sep(double z)
+    {
+        if (z < 0.0) z = 0.0;
+        if (z > 1.0) z = 1.0;
+        return ((1 - FieldDepth * z) * (2 * separation) / (2 - FieldDepth * z));
+    }
+
+    /// <summary>
+    /// Generate the stereogram.  Does all the common functionality, then calls the delegate
+    /// set by the subclass to do the actual work.
+    /// </summary>
+    public Image Generate(Image depthMap, Image texture)
+    {
+        var textureWidth = (int)separation;
+        var textureHeight = (int)((separation * texture.Height) / texture.Width);
+
+        var timer = new System.Diagnostics.Stopwatch();
+        timer.Start();
+
+        _pixels = new Image<Rgb24>(lineWidth, rows);
+
+        _texturePixels = texture
+            .CloneAs<Rgb24>()
+            .Clone(op => op.Resize(textureWidth, textureHeight));
+
+        _depthBytes = depthMap
+            .CloneAs<A8>()
+            .Clone(op => op.Resize(_depthWidth, resolutionY));
+
+        GeneratedLines = 0;
+
+        // Prime candidate for Parallel.For... yes, about doubles the speed of generation on my Quad-Core
+        if (System.Diagnostics.Debugger.IsAttached)   // Don't run parallel when debugging
+        {
+            for (var y = 0; y < rows; y++)
+            {
+                DoLine(y);
+                if (y > GeneratedLines)
+                {
+                    GeneratedLines = y;
+                }
+            }
+        }
+        else
+        {
+            Parallel.For(0, rows, y =>
+           {
+               if (false == abort)
+               {
+                   DoLine(y);
+               }
+               if (y > GeneratedLines)
+               {
+                   GeneratedLines = y;
+               }
+           });
+        }
+
+        return _pixels;
+    }
+}
+
+/// <summary>
+/// Generate a stereogram using the Horoptic algorithm.
+/// Algorithm Copyright 1996-2012 Simon Booth
+/// http://machinewrapped.wordpress.com/stereogrammer/
+/// </summary>
+internal class StereogramGeneratorHoroptic : StereogramGenerator
+{
+    protected int[] centreOut;
+
+    private int _hidden = 0;
+
+    public StereogramGeneratorHoroptic(Options options)
+        : base(options)
+    {
+        // Create an array of offsets which alternate pixels from the center out to the edges
+        centreOut = new int[lineWidth];
+        var offset = midpoint;
+        var flip = -1;
+        for (var i = 0; i < lineWidth; i++)
+        {
+            centreOut[i] = offset;
+            offset += ((i + 1) * flip);
+            flip = -flip;
+        }
+    }
+
+    protected override void DoLine(int y)
+    {
+        // Set up a constraints buffer with each pixel initially constrained to equal itself (probably slower than a for loop but easier to step over :p)
+        // And convert depths to floats normalised 0..1
+        var constraints = new int[lineWidth];
+        var depthLine = new float[lineWidth];
+        var maxDepth = 0.0f;
+
+        for (var i = 0; i < lineWidth; i++)
+        {
+            constraints[i] = i;
+            depthLine[i] = GetDepthFloat(i, y);
+        }
+
+        // Process the line updating any constrained pixels
+        for (var ii = 0; ii < lineWidth; ii++)
+        {
+            // Work from centre out
+            var i = centreOut[ii];
+
+            // Calculate Z value of the horopter at this x,y. w.r.t its centre.  20 * separation is approximation of distance to viewer's eyes
+            var zh = Math.Sqrt(SquareOf(20 * separation) - SquareOf(i - midpoint));
+
+            // Scale to the range [0,1] and adjust to displacement from the far plane
+            zh = 1.0 - (zh / (20 * separation));
+
+            // Separation of pixels on image plane for this point
+            // Note - divide ZH by FieldDepth as the horopter is independant
+            // of field depth, but sep macro is not.
+            var s = (int)Math.Round(Sep(depthLine[i] - (zh / FieldDepth)));
+
+            var left = i - (s / 2);           // The pixel on the image plane for the left eye
+            var right = left + s;           // And for the right eye
+
+            if ((0 <= left) && (right < lineWidth))                     // If both points lie within the image bounds ...
+            {
+                var visible = true;
+                if (RemoveHiddenSurfaces)                   // Perform hidden surface test (if requested)
+                {
+                    var t = 1;
+                    double zt = depthLine[i];
+                    var delta = 2 * (2 - FieldDepth * depthLine[i]) / (FieldDepth * separation * 2);   // slope of line of sight
+                    do
+                    {
+                        zt += delta;
+                        visible = (depthLine[i - t] < zt) && (depthLine[i + t] < zt);           // False if obscured on left or right (can only be obscured by innermost one)
+                        t++;
+                    }
+                    while (visible && zt < maxDepth);  // cache the max depth of the line to minimise checks needed
+                }
+                if (visible)
+                {
+                    // Decide whether we want to constrain the left or right pixel
+                    // Want to avoid constraint loops, so always constrain outermost pixel to innermost
+                    // Should depend if one or the other is already constrained I suppose
+                    var constrainee = Outermost(left, right, midpoint);
+                    var constrainer = (constrainee == left) ? right : left;
+
+                    // Find an unconstrained pixel and constrain ourselves to it
+                    // Uh-oh, what happens if they become constrained to each other?  Constrainee is flagged as unconstrained, I suppose
+                    while (constraints[constrainer] != constrainer)
+                        constrainer = constraints[constrainer];
+
+                    constraints[constrainee] = constrainer;
+                }
+                else
+                {
+                    _hidden++;
+                }
+
+                // Points can only be hidden by a point closer to the centre, i.e. one we've already processed
+                if (depthLine[i] > maxDepth)
+                    maxDepth = depthLine[i];
+            }
+        }
+
+        // Now actually set the pixels
+        for (var i = 0; i < lineWidth; i++)
+        {
+            var pix = i;
+
+            // Find an unconstrained pixel
+            while (constraints[pix] != pix)
+                pix = constraints[pix];
+
+            // And get the RGBs from the tiled texture at that point
+            SetStereoPixel(i, y, GetTexturePixel(pix, y));
+        }
+    }
+}
